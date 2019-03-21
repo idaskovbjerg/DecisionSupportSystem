43f90303b46d5dde93d3625a9e35a373aee6ce94
Ida Mark Skovbjerg <ida_skovbjerg@hotmail.com>
2019-03-21 18:35:30 +0100

    Exercises

---
+++ Øvelser/MetricsUtil.py
@@ -0,0 +1,30 @@
+import math
+import numpy as np
+
+#Mean squared error
+def Mse(Y, Y_hat):
+    #return (1.0 / n) * ((Y_hat- Y) ** 2).sum()
+    return ((Y_hat- Y) ** 2).mean()
+
+#Residual sum of squares
+def RSS(Y, Y_hat):
+    residual = Y - Y_hat
+    return (residual ** 2).sum()
+
+#Adjusted R^2
+def A_rsquare(RSS, Y, Y_hat, n, d):
+    #Total sum of squares, formula: sum((y-y_mean)^2)
+    TSS = ((Y - Y.mean()) ** 2).sum()
+    #Formula from slides:  adjusted R^2 = 1-(RSS/(n-d-1)/(TSS/(n-1)))
+    return 1.0 - ((RSS / (n - d - 1)) / (TSS / (n - 1)))
+
+#Mallows Cp
+def Cp(RSS, d, Y_hat, n):
+    # Formula from slides: Cp = 1/n * (RSS + 2* d * sigma^2)
+    return  (1.0 / n) * (RSS + 2 * d * Y_hat.var())
+      
+#Natural logaritmic model: BIC	  
+def Bic(n, RSS, d, Y_hat):
+      # Formula from slides: bic = 1/n * (RSS+log(n)*d*sigma^2)
+      return (1.0 / n) * (RSS + math.log(n) * d * Y_hat.var())
+      
